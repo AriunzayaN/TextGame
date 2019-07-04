05de89dfd610221f72662e6f237645acae4aefff
AriunzayaN <anamkhainyambuu@gmail.com>
2019-07-03 21:50:08 -0700

    Entity generator implemented

---
+++ manager.cpp
@@ -0,0 +1,32 @@
+#include "manager.h"
+#include "entities.h"
+
+using namespace std;
+
+Entity* Manager::gen(string entityName, int& id){
+    Entity* ptr; //nullptr
+    if(entityName == "Axe"){
+        ptr = new Axe();
+    }else if(entityName == "Crate"){
+        ptr = new Crate();
+    }else if(entityName == "Key"){
+        ptr = new Key();
+    }
+    id = size();
+    allitems.push_back(ptr);
+    return ptr;
+};
+int     Manager::size(){
+    return allitems.size();
+};
+void    Manager::free(int id){
+    delete allitems[id];
+};
+Manager::~Manager(){
+    for (int i = 0; i < size(); i++)
+    {
+        if(allitems[i] != nullptr){
+            delete allitems[i];
+        }
+    }
+};
